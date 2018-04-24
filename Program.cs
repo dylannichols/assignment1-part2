755812922b4dac0293c1ded44951c72b3f9c36e9
dylannichols <nichodo@g.toiohomai.ac.nz>
2018-04-24 16:24:45 +1200

    release 1.0

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace assignment1_part2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var newCar = new Car();
+            Console.WriteLine("Enter new car details below");
+            Console.Write("Colour: ");
+            newCar.Colour = Console.ReadLine();
+
+            Console.Write("Make: ");
+            newCar.Make = Console.ReadLine();
+
+            Console.Write("Gearbox type: ");
+            newCar.Gearbox = Console.ReadLine();
+
+            Console.WriteLine(newCar.DescribeCar());
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public string Colour {get; set;}
+        public string Make {get; set;}
+        public string Gearbox {get; set;}
+
+        public string DescribeCar()
+        {
+            return $"\nThis car is a {Colour} {Make} with a {Gearbox} transmission";
         }
     }
 }
